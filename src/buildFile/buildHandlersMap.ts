c1d1fa4a842a57a0a31c8d8ce7780e1be933904c
Tom Middlehurst <25036538+tmiddlehurst@users.noreply.github.com>
2024-11-01 15:16:00 +0000

    mirage-safe paths with path params

--- src/buildFile/buildHandlersMap.ts
+++ src/buildFile/buildHandlersMap.ts
@@ -1,13 +1,19 @@
 import { HandlerConfig } from '../getRouteHandlerConfig';
 import { AutogenComment } from '../utils';
 
+// Replace URL path params with mirage path param syntax
+// e.g. /coffee/{cupId} -> /coffee:cupId
+function miragePath(path: string) {
+  return path.replace(/\/\{([^}]+)\}/g, ':$1');
+}
+
 export default function (handlers: HandlerConfig[]): string {
   const imports = handlers.reduce((fileString, handler) => {
     return `import ${handler.name} from \'./handlers/${handler.name}\'\n${fileString}`;
   }, '');
 
   const mapPuts = handlers.reduce((fileString, handler) => {
-    const config = { verb: handler.method, path: handler.path };
+    const config = { verb: handler.method, path: miragePath(handler.path) };
     return `handlersMap.set(${JSON.stringify(config)}, ${handler.name})\n${fileString}`;
   }, '');
 
--- tests/buildHandlersMap.test.ts
+++ tests/buildHandlersMap.test.ts
@@ -4,38 +4,57 @@ import { format } from '../src/utils';
 import { HandlerConfig } from '../src/getRouteHandlerConfig';
 import { AutogenComment } from '../src/utils';
 
-const HANDLER_CONFIG: HandlerConfig[] = [
-  { name: 'getDonutsHandler', path: '/donuts', method: 'get' },
-  { name: 'addDonutHandler', path: '/donuts', method: 'post' },
-  { name: 'pourCoffeeHandler', path: '/coffee', method: 'put' },
-  { name: 'drinkCoffeeHandler', path: '/coffee', method: 'delete' },
-];
-
-const EXPECTED = `
-${AutogenComment}
-import drinkCoffeeHandler from './handlers/drinkCoffeeHandler';
-import pourCoffeeHandler from './handlers/pourCoffeeHandler';
-import addDonutHandler from './handlers/addDonutHandler';
-import getDonutsHandler from './handlers/getDonutsHandler';
-
-export type HandlerRequest = {
-  verb: 'get' | 'post' | 'put' | 'patch' | 'delete';
-  path: string;
-};
-export type MirageRouteHandler = (schema: any, request: Request) => any;
-
-const handlersMap = new Map<HandlerRequest, MirageRouteHandler>;
-handlersMap.set({ verb: \"delete\", path: \"/coffee\" }, drinkCoffeeHandler);
-handlersMap.set({ verb: \"put\", path: \"/coffee\" }, pourCoffeeHandler);
-handlersMap.set({ verb: \"post\", path: \"/donuts\" }, addDonutHandler);
-handlersMap.set({ verb: \"get\", path: \"/donuts\" }, getDonutsHandler);
-
-export default handlersMap;`;
-
 describe('building map of route handlers', () => {
   test('builds map of handlers', async () => {
-    const expected = await format(EXPECTED);
-    const input = await format(buildHandlersMap(HANDLER_CONFIG));
+    const handlers: HandlerConfig[] = [
+      { name: 'getDonutsHandler', path: '/donuts', method: 'get' },
+      { name: 'addDonutHandler', path: '/donuts', method: 'post' },
+      { name: 'pourCoffeeHandler', path: '/coffee', method: 'put' },
+      { name: 'drinkCoffeeHandler', path: '/coffee', method: 'delete' },
+    ];
+
+    const expected = await format(`
+      ${AutogenComment}
+      import drinkCoffeeHandler from './handlers/drinkCoffeeHandler';
+      import pourCoffeeHandler from './handlers/pourCoffeeHandler';
+      import addDonutHandler from './handlers/addDonutHandler';
+      import getDonutsHandler from './handlers/getDonutsHandler';
+
+      export type HandlerRequest = {
+        verb: 'get' | 'post' | 'put' | 'patch' | 'delete';
+        path: string;
+      };
+      export type MirageRouteHandler = (schema: any, request: Request) => any;
+
+      const handlersMap = new Map<HandlerRequest, MirageRouteHandler>;
+      handlersMap.set({ verb: \"delete\", path: \"/coffee\" }, drinkCoffeeHandler);
+      handlersMap.set({ verb: \"put\", path: \"/coffee\" }, pourCoffeeHandler);
+      handlersMap.set({ verb: \"post\", path: \"/donuts\" }, addDonutHandler);
+      handlersMap.set({ verb: \"get\", path: \"/donuts\" }, getDonutsHandler);
+
+      export default handlersMap;`);
+    const input = await format(buildHandlersMap(handlers));
+    expect(input).toBe(expected);
+  });
+
+  test('replaces url path params with mirage path param syntax', async () => {
+    const expected = await format(` ${AutogenComment}
+      import pourCoffeeHandler from './handlers/pourCoffeeHandler';
+
+      export type HandlerRequest = {
+        verb: 'get' | 'post' | 'put' | 'patch' | 'delete';
+        path: string;
+      };
+      export type MirageRouteHandler = (schema: any, request: Request) => any;
+
+      const handlersMap = new Map<HandlerRequest, MirageRouteHandler>;
+      handlersMap.set({ verb: \"put\", path: \"/coffee:cupId\" }, pourCoffeeHandler);
+
+      export default handlersMap;`);
+    const handlers: HandlerConfig[] = [
+      { name: 'pourCoffeeHandler', method: 'put', path: '/coffee/{cupId}' }
+    ];
+    const input = await format(buildHandlersMap(handlers));
     expect(input).toBe(expected);
   });
 });
